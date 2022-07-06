29330f9b9925352351e55fa0c1c7b675cdc87c6d
Summer Jade <summerjadefrosal@gmail.com>
2022-07-05 23:35:56 -0400

    added saveBtn function and local storage

--- Assets/script.js
+++ Assets/script.js
@@ -5,10 +5,10 @@ var saveBtn = $(".saveBtn");
 // current day will be displayed at the top of the calendar //
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
-// each time-block will be color coded to indicate if it is in the past, present, or future //
+// each time block will be color coded to indicate if it is in the past, present, or future //
 function timeBlockColor() {
     var currentHour = moment().hours();
-$(".timeBlock").each(function(){
+    $(".timeBlock").each(function(){
     var timeBlockHour = parseInt($(this).attr('id'));
 
     //console.log(this); 'each time-block//
@@ -20,8 +20,19 @@ $(".timeBlock").each(function(){
     } else {
         $(this).addClass("past");
     }
-})
+    })
 };
+
+// WHEN I click the save button for that time block
+saveBtn.on("click", function() {
+
+    // console.log(this); //save button
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(time, plan);
+});
 
 // CALL FUNCTIONS //
 
