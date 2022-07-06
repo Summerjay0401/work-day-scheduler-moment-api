e50ab71e037b7760fb517d6363c1be14c8b68c75
Summer Jade <summerjadefrosal@gmail.com>
2022-07-05 23:29:43 -0400

    added javascript function for timeBlockColor

---
+++ Assets/script.js
@@ -0,0 +1,28 @@
+var saveBtn = $(".saveBtn");
+
+// FUNCTIONS //
+
+// current day will be displayed at the top of the calendar //
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+// each time-block will be color coded to indicate if it is in the past, present, or future //
+function timeBlockColor() {
+    var currentHour = moment().hours();
+$(".timeBlock").each(function(){
+    var timeBlockHour = parseInt($(this).attr('id'));
+
+    //console.log(this); 'each time-block//
+
+    if (timeBlockHour > currentHour) {
+        $(this).addClass("future");
+    } else if (timeBlockHour === currentHour){
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("past");
+    }
+})
+};
+
+// CALL FUNCTIONS //
+
+timeBlockColor();
